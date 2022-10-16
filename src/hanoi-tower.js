bcf2d519f5b616b575acb484f1d2aec12e3441e5
k2u6m5i3r <wunder49@yandex.by>
2022-10-16 21:25:27 +0300

    add: tasks add two

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,9 +14,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
+function calculateHanoi(disksNumber, turnsSpeed ) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  // количество перекладываний по формуле 2^n -1 ;-)
+  return {
+    turns:(Math.pow(2,disksNumber)-1),
+    seconds:Math.floor((Math.pow(2,disksNumber)-1)/turnsSpeed*3600),
+}
 }
 
 module.exports = {
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,10 +11,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+
+function sortByHeight(arr) {
+  //throw new NotImplementedError("Not implemented");
   // remove line with error and write your code here
+  let sortedArray = arr.filter(function (item) {
+    if (item != -1) {
+      return item;
+    }
+  }).sort(function (a, b) {
+    return a - b;
+  });
+  let ans = [];
+  for (let i = 0, j = 0; i < arr.length; i++) {
+    if (arr[i] == -1) {
+      ans.push(arr[i]);
+    } else {
+      ans.push(sortedArray[j++]);
+    }
+  }
+  return ans;
 }
+
 
 module.exports = {
   sortByHeight
