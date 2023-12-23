894274c477d47347ef0570ea9883da6f2f3405cd
Sergei Shevchenko <wunder49@yandex.by>
2023-12-23 21:48:39 +0300

    remove solutions What-sesson and transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,8 +13,8 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(arr) {
-  // throw new NotImplementedError('Not implemented');
+function transform( /*arr*/) {
+  throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
   if (!Array.isArray(arr)) {
     throw new Error('\'arr\' parameter must be an instance of the Array!');
--- src/what-season.js
+++ src/what-season.js
@@ -11,8 +11,8 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(date) {
-  //throw new NotImplementedError('Not implemented');
+function getSeason(/* date */) {
+  throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
   if (!(date instanceof Date)) {
     throw new Error('Invalid date!');
