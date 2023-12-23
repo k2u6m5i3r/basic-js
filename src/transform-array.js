331ebad7f89cee65f7456c7586e283dcbe3c1d18
Sergei Shevchenko <wunder49@yandex.by>
2023-12-23 22:36:04 +0300

    add semi-solution transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,23 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform( /*arr*/) {
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
   if (!Array.isArray(arr)) {
-    throw new Error('\'arr\' parameter must be an instance of the Array!');
+    throw new Error("'arr' parameter must be an instance of the Array!");
   }
   if (arr.length == 0) {
     return [];
@@ -25,30 +25,26 @@ function transform( /*arr*/) {
   let ans = [];
   for (let index = 0; index < arr.length; index++) {
     const element = arr[index];
-    if (arr[index] == '--discard-prev') {
+    if (arr[index] == "--discard-prev") {
       if (index == 0) {
-
       } else {
         ans.push(element);
       }
     }
-    if (arr[index] == '--double-prev') {
+    if (arr[index] == "--double-prev") {
       if (index == 0) {
-
       } else {
         ans.push(element);
       }
     }
-    if (arr[index] == '--double-next') {
-      if (index == arr.length-1) {
-
+    if (arr[index] == "--double-next") {
+      if (index == arr.length - 1) {
       } else {
         ans.push(element);
       }
     }
-    if (arr[index] == '--discard-next') {
-      if (index == arr.length-1) {
-
+    if (arr[index] == "--discard-next") {
+      if (index == arr.length - 1) {
       } else {
         ans.push(element);
       }
@@ -60,5 +56,5 @@ function transform( /*arr*/) {
 }
 
 module.exports = {
-  transform
+  transform,
 };
