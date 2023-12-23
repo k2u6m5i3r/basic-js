fa7c3f0f98ec55511ad511bd6ab90909d925df1b
Sergei Shevchenko <wunder49@yandex.by>
2023-12-23 22:01:35 +0300

    refactor dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,14 +1,14 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
@@ -16,24 +16,25 @@ const { NotImplementedError } = require('../extensions/index.js');
 function createDreamTeam(members) {
   // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
-  if(Array.isArray(members)){
-    return members.filter(function (item) {
-      if (typeof (item) == 'string') {
+  if (Array.isArray(members)) {
+    return members
+      .filter(function (item) {
+        if (typeof item == "string") {
           return true;
-      }
-  }
-  ).map(function (item){
-    return item.toUpperCase().trim();
-  }
-  ).sort().reduce(function (sum, current) {
-      return sum + current[0];
-  }, "");
-  }else{
+        }
+      })
+      .map(function (item) {
+        return item.toUpperCase().trim();
+      })
+      .sort()
+      .reduce(function (sum, current) {
+        return sum + current[0];
+      }, "");
+  } else {
     return false;
   }
-
 }
 
 module.exports = {
-  createDreamTeam
+  createDreamTeam,
 };
