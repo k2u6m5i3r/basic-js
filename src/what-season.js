c391b5ee7863116ff2b335e72a5ae4a9b7023304
Sergei Shevchenko <wunder49@yandex.by>
2023-12-23 22:32:22 +0300

    add solution for what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,46 +1,56 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
+
+function getSeason(date = undefined) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
-  if (!(date instanceof Date)) {
-    throw new Error('Invalid date!');
+  if (date == undefined) {
+    return "Unable to determine the time of year!";
   }
-  switch (date.getMonth()) {
+  
+  if (
+    !(date instanceof Date) ||
+    Object.prototype.toString.call(date) !== "[object Date]" ||
+    Object.getOwnPropertySymbols(date).length !== 0
+  ) {
+    throw new Error("Invalid date!");
+  }
+
+  switch (date.getMonth() + 1) {
     case 1:
     case 2:
-      return 'winter';
+      return "winter";
     case 3:
     case 4:
     case 5:
-      return 'spring';
+      return "spring";
     case 6:
     case 7:
     case 8:
-      return 'summer';
+      return "summer";
     case 9:
     case 10:
     case 11:
-      return 'autumn';
+      return "autumn";
     case 12:
-      return 'winter';
+      return "winter";
     default:
-      return "Invalid date!"
+      return "Invalid date!";
       break;
   }
 }
 
 module.exports = {
-  getSeason
+  getSeason,
 };
