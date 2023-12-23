8794a502c465acc2e5c954efb0abf29ef5298552
Sergei Shevchenko <wunder49@yandex.by>
2023-12-23 22:09:24 +0300

    refactoring mac-address

--- src/mac-address.js
+++ src/mac-address.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * The MAC-48 address is six groups of two hexadecimal digits (0 to 9 or A to F),
@@ -14,23 +14,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
- function isMAC48Address(n) {
+function isMAC48Address(n) {
   // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
-  let strToArr = n.split('-');
-  // console.log(strToArr)
-  let ans = true;
-  for (let i = 0; i < strToArr.length; i++) {
-      // console.log(parseInt(strToArr[i], 16));
-      // console.log(!Number.isNaN(parseInt(strToArr[i], 16)));
-      // console.log(parseInt(strToArr[i], 16) <= 0xff);
-      if ( parseInt(strToArr[i], 16) > 0xff || Number.isNaN( parseInt(strToArr[i], 16)) ) {
-          ans = false;
-          break;
-      }
-  }
-  return ans;
+  return (
+    n
+      .split("-")
+      .filter(
+        (item) =>
+          !parseInt(item, 16) > 0xff || !Number.isNaN(parseInt(item, 16))
+      ).length === 6
+  );
 }
 module.exports = {
-  isMAC48Address
+  isMAC48Address,
 };
